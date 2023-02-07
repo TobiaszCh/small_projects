0a33bb778c990a394404d758ed085b9407b888a5
Tobiasz <tobijas.ch@gmail.com>
2023-02-07 11:47:40 +0100

    ROT13 encryption code

---
+++ short-intresting-tasks/src/main/java/ROT13.java
@@ -0,0 +1,27 @@
+public class ROT13 {
+
+    public static String rot13(String message) {
+        String abc = "abcdefghijklm";
+        String nop = "nopqrstuvwxyz";
+        String ABC = abc.toUpperCase();
+        String NOP = nop.toUpperCase();
+        String[] messages = message.split("");
+        StringBuilder result = new StringBuilder();
+        for(String mess: messages) {
+            String MESS = mess.toUpperCase();
+            if(abc.contains(mess)) {
+                result.append(mess.replace(mess, String.valueOf(nop.charAt(abc.indexOf(mess)))));
+            } else if (ABC.contains(MESS)) {
+                result.append(MESS.replace(MESS, String.valueOf(NOP.charAt(ABC.indexOf(MESS)))));
+            } else if (nop.contains(mess)) {
+                result.append(mess.replace(mess, String.valueOf(abc.charAt(nop.indexOf(mess)))));
+            } else if (NOP.contains(MESS)) {
+                result.append(MESS.replace(MESS, String.valueOf(ABC.charAt(NOP.indexOf(MESS)))));
+            }
+            else {
+                result.append(mess);
+            }
+        }
+        return String.valueOf(result);
+    }
+}
