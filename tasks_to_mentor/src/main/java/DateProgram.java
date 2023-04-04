1cef35558efafee28b338a0a372f39c6255dd2f3
Tobiasz <tobijas.ch@gmail.com>
2023-04-04 13:17:55 +0200

    program display diffrent number of days, for example: to ChrsmasEve, since brith of Jesus e.c.t

---
+++ tasks_to_mentor/src/main/java/DateProgram.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class DateProgram {
+
+    private final String todayName;
+    private final long dayOfWeek;
+    private final long dayOfMonth;
+    private final long dayOfYear;
+    private final LocalDate nowDate;
+    private final LocalDate birthOfChrist;
+    private final LocalDate year2000;
+    private LocalDate christmasEve;
+
+    public DateProgram() {
+        this.todayName = LocalDate.now().getDayOfWeek().name().toLowerCase();
+        this.dayOfWeek = LocalDate.now().getDayOfWeek().getValue();;
+        this.dayOfMonth = LocalDate.now().getDayOfMonth();
+        this.dayOfYear = LocalDate.now().getDayOfYear();
+        this.birthOfChrist = LocalDate.of(1, 1, 1);
+        this.year2000 = LocalDate.of(2000, 1, 1);
+        this.christmasEve = LocalDate.of(LocalDate.now().getYear(), 12, 24);
+        this.nowDate = LocalDate.now();
+    }
+        public String getDifferentDates() {
+            String result;
+            long daysSinceBirthOfChrist = ChronoUnit.DAYS.between(birthOfChrist, nowDate);
+            long daysSinceYear2000 = ChronoUnit.DAYS.between(year2000, nowDate);
+            if (nowDate.isAfter(christmasEve)) {
+                christmasEve = christmasEve.plusYears(1);
+            }
+            long daysToChristmasEve = ChronoUnit.DAYS.between(nowDate, christmasEve);
+            if (Objects.equals(todayName, "monday") || Objects.equals(todayName, "tuesday")) {
+                result = "Day of week: " + dayOfWeek + "\n" + "Day of month: "
+                        + dayOfMonth + "\n" + "Day of year: " + dayOfYear + "\n" + "Days since birth of Christ " + daysSinceBirthOfChrist
+                        + "\n" + "Days since year 2000: " + daysSinceYear2000 + "\n" + "Days to Christmas Eve: " + daysToChristmasEve;
+            } else {
+                result = "This program work monday and thursday only";
+            }
+            return result;
+        }
+}
---
+++ tasks_to_mentor/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        DateProgram dateProgram = new DateProgram();
+        System.out.println(dateProgram.getDifferentDates());
+    }
+}
